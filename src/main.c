23dad7dc1ebfe36a1b157832fdc16a25fcfade0c
Sean Bryant <sbryant@github.com>
2014-02-23 12:12:45 -0800

    remove extract kinematic class

--- src/main.c
+++ src/main.c
@@ -11,110 +11,11 @@
 #define GRID_H 50
 #define GRID_W 50
 
-typedef struct s_static {
-    vec3 position;
-    float orientation;
-} Static;
-
-typedef struct s_kinematic {
-    vec3 position;
-    float orientation;
-    vec3 velocity;
-    float rotation;
-} Kinematic;
-
-typedef struct s_kinematic_steering_output {
-    vec3 velocity;
-    float rotation;
-} KinematicSteeringOutput;
-
-typedef struct s_steering_output {
-    vec3 linear;
-    float angular;
-} SteeringOutput;
-
-typedef struct s_kinematic_seek {
-    Static character;
-    Static target;
-    float max_speed;
-} KinematicSeek;
 
 typedef struct s_app_state {
     short quit;
 
 } AppState;
-
-KinematicSeek* kmseek_make(KinematicSeek *k) {
-    if(!k)
-        k = (KinematicSeek*)calloc(1, sizeof(KinematicSeek));
-
-    k->max_speed = 1.0f;
-    return k;
-}
-Static* static_make(Static *s) {
-    if(!s)
-        s = (Static*)calloc(1, sizeof(Static));
-
-    return s;
-}
-Kinematic* km_make(Kinematic *k) {
-    if(!k)
-        k = (Kinematic*)calloc(1, sizeof(Kinematic));
-
-    return k;
-}
-
-SteeringOutput* so_make(SteeringOutput *s) {
-    if(!s)
-        s = (SteeringOutput*)calloc(1, sizeof(SteeringOutput));
-
-    return s;
-}
-
-KinematicSteeringOutput* kso_make(KinematicSteeringOutput *s) {
-    if(!s)
-        s = (KinematicSteeringOutput*)calloc(1, sizeof(KinematicSteeringOutput));
-
-    return s;
-}
-
-float get_new_orientation(Static *s, float current, vec3 *velocity) {
-    if(vec3_length(velocity) > 0.0f) {
-        return atan2f(-s->position.x, s->position.z);
-    }
-    return current;
-}
-
-void km_update(Kinematic *input, SteeringOutput *steering, float time) {
-    vec3 new_pos, new_vel;
-    vec3_mul_scalar(&(input->velocity), time, &new_pos);
-    vec3_add(&(input->position), &new_pos, &(input->position));
-
-    input->orientation += input->rotation * time;
-
-    vec3_mul_scalar(&(steering->linear), time, &new_vel);
-    vec3_add(&(input->velocity), &new_vel, &(input->velocity));
-
-    input->rotation += steering->angular * time;
-
-};
-
-KinematicSteeringOutput* kmseek_get_steering(KinematicSeek* k) {
-    KinematicSteeringOutput *steering = kso_make(NULL);
-
-    /* Direction to Target */
-    vec3_sub(&(k->target.position), &(k->character.position), &(steering->velocity));
-
-    /* full speed along this direction */
-    vec3_normalize(&(steering->velocity), &(steering->velocity));
-    vec3_mul_scalar(&(steering->velocity), k->max_speed, &(steering->velocity));
-
-    /* face the correct direction we will be moving*/
-    k->character.orientation = get_new_orientation(&(k->character), k->character.orientation, &(steering->velocity));
-
-    steering->rotation = 0.0f;
-    return steering;
-}
 
 int KeyboardEventFilter(void *user_data, SDL_Event *event) {
     AppState *app = (AppState*)user_data;
